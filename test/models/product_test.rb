35eddfc45eafa3bd728f7c1cf1b8c26b20ac9046
transcendence12 <gosiek.krawczuk@gmail.com>
2025-01-03 14:52:27 +0100

    feat: add tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,16 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  include ActionMailer::TestHelper
+
+  test "sends email notifications when back in stock" do
+    product = products(:tshirt)
+
+    # Set product out of stock
+    product.update(inventory_count: 0)
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
